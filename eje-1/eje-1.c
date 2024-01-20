2766fa5612bee6f60502ec99eeda67a6f1be14a1
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-01-20 13:04:08 -0300

    ejercicio 1 terminado

---
+++ eje-1/eje-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void reverse(int n){
+    int reverse = 0;
+    int cantidad_impares = 0;
+
+    while (n != 0)
+    {
+        int digito = n % 10;
+        if (digito % 2 != 0)
+        {
+            cantidad_impares++;
+        }
+        reverse = reverse * 10 + digito;
+        n /= 10;
+    }
+    printf("La cantidad de digitos impares es: %d\n", cantidad_impares);
+    printf("El numero invertido es: %d\n", reverse);
+}
+
+
+int main(){
+    int numeros;
+
+    printf("Ingrese un numero: ");
+    scanf("%d", &numeros);
+
+    while (numeros != 0)
+    {
+        reverse(numeros);
+        printf("Ingrese un numero: ");
+        scanf("%d", &numeros);
+    }
+    
+    return 0;
+}
